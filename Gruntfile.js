e0c9917c5fc26e7a47b6d37e0b59532aa0d8f1d3
Nick Colley <nickcolley7@gmail.com>
2014-02-16 23:03:27 +0000

    added comments and cleaned it up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,24 @@
 module.exports = function(grunt) {
- 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+
+	//Loads the modules we will be using for the different tasks. 
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-usemin');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
+	//This config is loaded when grunt is ran.
 	grunt.initConfig({
+		//This generates a config from your index.html
 		useminPrepare: {
 			html: 'index.html',
 			options: {
 				dest: 'dist'
 			}
 		},
+		//We want to copy the other assets into the build directory
 		copy: {
 			main: {
 				src: [
@@ -29,12 +33,14 @@ module.exports = function(grunt) {
 				dest: 'dist/'
 			},
 		},
+		//This alters your index.html to point at the built assets
 		usemin: {
 			html: 'dist/index.html',
 			options: {
 				assetsDirs: ['css', 'js']
 			}
 		},
+		//General image compression
 		imagemin: {
 			main: {
 				files: [{
@@ -46,14 +52,20 @@ module.exports = function(grunt) {
 			}
 		}
 	});
- 
-	grunt.registerTask('default', [
-									'useminPrepare',
-									'copy',
-									'concat',
-									'uglify',
-									'cssmin',
-									'usemin',
-									'imagemin'
-								]);
+
+	//Our tasks in the order they should be ran. Note that concat, uglify and others
+	//are run on the generated config from usemin.
+	var default_tasks =
+	[
+		'useminPrepare',
+		'copy',
+		'concat',
+		'uglify',
+		'cssmin',
+		'usemin',
+		'imagemin'
+	]
+
+	//registers the task that wull be run when 'grunt' is called from the cli 
+	grunt.registerTask('default', default_tasks);
 };
