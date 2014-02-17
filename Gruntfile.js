e329c3a2fb4d23a93c713d1747ece208016c8ba7
Martin Evans <martindevans@gmail.com>
2014-02-17 04:15:27 +0000

    just copy the whole damn galleria folder tree over
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
 						'icons/*/**',
 						'img/*',
 						'fonts/*',
-                        'galleria/*'
+                        'galleria/**'
 					 ],
 				dest: 'dist/'
 			},
